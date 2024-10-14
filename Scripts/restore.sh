b6165f3058a8c60e75948fe3e5d142b358aeda9f
Anguram Shanmugam <anguram.k.s@gmail.com>
2024-10-14 18:20:37 +0530

    new backup 14-10-2024

--- Scripts/restore.sh
+++ Scripts/restore.sh
@@ -8,11 +8,11 @@ cp -r $HOME/.dotfiles/VSCode/settings.json $HOME/AppData/Roaming/Code/User/
 cp -r $HOME/.dotfiles/VSCode/keybindings.json $HOME/AppData/Roaming/Code/User/
 
 # Restore VSCode `extensions`
-extensions_file="$HOME/.dotfiles/VSCode/extensions"
+# extensions_file="$HOME/.dotfiles/VSCode/extensions.json"
 
-for extension_id in $(cat "$extensions_file"); do
-    code --install-extension $extension_id
-done
+# for extension_id in $(cat "$extensions_file"); do
+#     code --install-extension $extension_id
+# done
 
 # Restore Hyper Terminal `.hyper.js`
 cp -r $HOME/.dotfiles/Hyper/.hyper.js $HOME/AppData/Roaming/Hyper/
